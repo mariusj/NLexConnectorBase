d838e8cee45a7d7698019eeaaced6af954078e25
Mariusz Jakubowski <mariusz.jakubowski.79@gmail.com>
2019-01-08 15:57:49 +0100

    added enum with error codes

---
+++ src/main/java/eu/europa/eurlex/nlex/ErrorCode.java
@@ -0,0 +1,18 @@
+package eu.europa.eurlex.nlex;
+
+/**
+ * A list of well known error codes of the connector.
+ * @author Mariusz Jakubowski
+ *
+ */
+public enum ErrorCode {
+    NO_ERROR,
+    NO_CRITERIA,
+    TOO_MANY_RESULTS,
+    INCORRECT_CRITERIA,
+    DB_UNAVAILABLE,
+    EXCLUSIVE_CRITERIA,
+    INCORRECT_RESULT,
+    DOC_NOT_FOUND,
+    INVALID_REQUEST_ID
+}
